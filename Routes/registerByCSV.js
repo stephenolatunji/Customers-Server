fb3a3c3da9865f55b8ca45e3181088f7b115fbfe
Stephen <stephenolatunji02@gmail.com>
2021-10-25 10:48:45 +0100

    Bug fix

--- Routes/registerByCSV.js
+++ Routes/registerByCSV.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const csv = require('csvtojson');
 const multer = require('multer')
 const fs = require('fs');
-// const { json } = require('express');
 const randomize = require('randomatic');
 
 global.__basedir = __dirname;
@@ -33,13 +32,13 @@ const upload = multer({storage: parser, fileFilter: filter});
 
 router.route('/upload')
     .post(upload.single('file'), async(req, res) =>{
-        res.setHeader('Content-Type', 'multipart/form-data');
+        // res.setHeader('Content-Type', 'multipart/form-data');
         try{
             if(req.file == undefined){
                 return res.status(400).json({success: false, msg: 'Please upload a CSV file'});
             }
           else {  let filePath = __basedir + '/uploads/' + req.file.filename;
-         
+            var nonExistingUser = 0;
            
             const date = new Date().getFullYear()+'-'+(new Date().getMonth()+parseInt("1"))+'-'+new Date().getDate();
             const dataArray = await csv().fromFile(filePath);
@@ -59,22 +58,21 @@ router.route('/upload')
                const country = singleData.country.toString(); 
                
                const random = randomize('0', 4);
-               console.log('random');
                const split_name = compName.slice(0, 3).toUpperCase();
                const split_type = type.charAt(0).toUpperCase();
 
                const code = `${split_type}${split_type}${split_name}${random}`;
-                let nonExistingUser = 0;
-                console.log(code);
+                
                 await connectDB.query(`SELECT COUNT(SF_Code) AS count FROM cust_tb WHERE SF_Code = '${salesforceCode}'`, async(err, result)=>{
                     nonExistingUser += result.recordset[0].count ? 0 : 1
+                    
                     if(!result.recordset[0].count){
                         await connectDB.query(
                             `INSERT INTO cust_tb (DIST_Code, BB_Code, SF_code,
                             CUST_type, CUST_Name, country, email, status, district, 
-                            region, address, phoneNumber, lat, long, registeredOn)
+                            region, address, phoneNumber, latitude, longitude, registeredOn)
                             VALUES('${sysproCode}', '${code}', '${salesforceCode}', '${type}', '${compName}', '${country}',
-                            '${email}','Active', '${district}', '${region}', '${address}', , 
+                            '${email}','Active', '${district}', '${region}', '${address}', 
                             '${Owner_Phone}', '${lat}', '${long}', '${date}' )`, (err, result) =>{
                                 if(err){
                                    return res.status(400).json({success: false, msg: 'Can not register customers', err});
