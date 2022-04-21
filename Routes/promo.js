8efd4142dbf3ab6672c9c1ed5e657a03380f07a0
Stephen <stephenolatunji02@gmail.com>
2022-04-21 01:05:15 +0100

    ...

--- Routes/promo.js
+++ Routes/promo.js
@@ -1,3 +1,48 @@
 const express = require('express');
 const connectDB = require('../config/db');
 const router = express.Router();
+
+router.route('/create-dream')
+    .post(async(req, res) =>{
+        const BB_Code = req.body.BB_Code;
+        const dreamName = req.body.dreamName;
+        const dreamPoint = req.body.dreamPoint;
+        const dreamDuration = req.body.dreamDuration;
+        const country = req.body.country;
+        const date = new Date().getFullYear()+'-'+(new Date().getMonth()+parseInt("1"))+'-'+new Date().getDate();
+
+        try{
+            await connectDB.query(`EXEC createDream @country = '${country}', @BB_Code = '${BB_Code}',
+            @dreamName = '${dreamName}', @dreamPoint = '${dreamPoint}', @dreamDuration = '${dreamDuration}',
+            @date = '${date}'`, (err, result) =>{
+                    if(result.recordset.length > 0){
+                        res.status(200).json({success: true, results: result.recordset[0]})
+                    }
+                    else{
+                        res.status(400).json({success: false, msg: 'Error creating dream'})
+                    }
+                })
+        }
+        catch(err){}
+    })
+
+    router.route('/getdream')
+    .post(async(req, res) =>{
+        const BB_Code = req.body.BB_Code;
+        const country = req.body.country;
+
+        try{
+            await connectDB.query(`EXEC getMyDream @BB_Code = '${BB_Code}', @country = '${country}'`, (err, results) =>{console.log(results);
+                if(results.recordset.length > 0){;
+                   return res.status(200).json({success: true, msg: 'Customers found!', results: results.recordset})
+                }
+                else{
+                    return res.status(404).json({success: false, msg: 'Customer with code not found'})
+                }
+            })
+
+        }catch(err){
+            res.status(500).json({success: false, err})
+        }
+    })
+    module.exports = router;
