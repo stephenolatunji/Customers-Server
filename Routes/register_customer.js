c159e6873c7cfca6451f49b763bd3436b48039c4
Stephen <stephenolatunji02@gmail.com>
2022-08-06 10:40:40 +0100

    Bug fix

--- Routes/register_customer.js
+++ Routes/register_customer.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('../config/db');
 const router = express.Router();
 const randomize  = require('randomatic');
+// const axios = require('axios');
 
 
 router.route('/')
@@ -28,8 +29,8 @@ router.route('/')
 
             const code = `${split_type}${split_type}${split_name}${random}`;
         try{
-            // await connectDB.query(`EXEC selectCountSFCode @salesforceCode = '${SFCode}'`, async(err, results) =>{
-            //     if(!results.recordset[0]['']){
+            await connectDB.query(`SELECT COUNT(phoneNumber) AS count FROM cust_tb WHERE phoneNumber = '${phone}'`, async(err, results) =>{
+                if(!results.recordset[0].count){
                   const result = await connectDB.request()
                   .input("distributorCode", distCode)
                   .input("salesforceCode", SFCode)
@@ -47,19 +48,48 @@ router.route('/')
                   .input("long", longitude)
                   .input("registeredOn", date)
                   .execute("registerCustomer");
-                    console.log(result);
                   if(result.recordset && result.recordset.length > 0){
-                    return res.status(401).json({success: true, msg: 'Customer registered successfully', results: result.recordset[0]});
+
+                    // const token = `POAPPLDMS2:Sec##urity123`;
+                    // const encodedToken = Buffer.from(token).toString('base64');
+                    // const dat = {
+                    //     SF_Code: SFCode,
+                    //     DIST_Code: distCode,
+                    //     CUST_Type: custType,
+                    //     CUST_Name: name,
+                    //     email: email,
+                    //     phoneNumber: phone,
+                    //     country: country,
+                    //     address: address,
+                    //     district: district,
+                    //     region: region,
+                    //     longitude: longitude,
+                    //     latitude: latitude,
+                    //     registeredOn: date,
+                    //     status: 'new',
+                    //     state: state
+                    // }
+                    // const data = dat.toArray()
+                    // axios.post('https://sappoqa.ab-inbev.com/RESTAdapter/Customer/CustomerCreate', data, 
+                    // {  headers: {'Authorization': `Basic ${encodedToken}`, 
+                    // 'content-type': 'application/json'}}).then(res => {
+                    //     console.log(res);
+                    //     res.status(200).json({success: true, msg: 'Customer registered successfully', results: res});
+                    //   })
+                    //   .catch(error => {
+                    //     res.status(500).json({success: false, msg: 'Failed to create customer on SalesForce', error})
+                    //   });
+                      return res.status(200).json({success: true, msg: 'Customer registered successfully', results: result.recordset[0]});
                   }
                   else{
-
+                    return res.status(400).json({success: false, msg: err})
                   }
                    
-            //     }
-            //     else{
-            //         return res.status(401).json({success: false, msg: 'Customer already registered', err});
-            //     }
-            // })
+                }
+                else{
+                    return res.status(401).json({success: false, msg: 'Customer with same phone number already registered', err});
+                }
+            })
         }
         catch(err){
             res.status(500).json({success: false, msg: 'Server Error', err});
